2bf63c07e3baaa7e4e6816d2c282406eecd65fe3
mccdo@iastate.edu <mccdo@iastate.edu@00c3c29d-d336-5f76-5ff7-ebc043f3ed38>
2012-09-04 20:32:16 +0000

    license updates.

--- src/switchwire/ConnectionMonopoly.cxx
+++ src/switchwire/ConnectionMonopoly.cxx
@@ -16,9 +16,7 @@
  * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
  * Boston, MA 02111-1307, USA.
  *
- *
  *************** <auto-copyright.rb END do not edit this line> ***************/
-
 #include <switchwire/ConnectionMonopoly.h>
 
 
