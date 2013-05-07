9d56b1922b4c157f17336f3f1b842a358f36687f
rpenn3@gmail.com <rpenn3@gmail.com@00c3c29d-d336-5f76-5ff7-ebc043f3ed38>
2013-05-07 19:51:41 +0000

    Fixes compiler warning and adds remaining ConnectScript templates.

--- src/switchwire/squirrel/ConnectScripts.h
+++ src/switchwire/squirrel/ConnectScripts.h
@@ -9,73 +9,99 @@ DIAG_OFF(unused-parameter)
 #include <sqrat/sqratVM.h>
 DIAG_ON(unused-parameter)
 
-#define SW_REPETETIVE_SCRIPT_CONNECTIONCODE Sqrat::DefaultVM::Set( vm.getVM() );\
-\
-try\
-{\
-    Sqrat::Script script;\
-    script.CompileFile( scriptPath );\
-    script.Run();\
-}\
-catch( Sqrat::Exception& e )\
-{\
-    std::cout << "Sqrat exception: " << e.Message() << std::endl << std::flush;\
-    return;\
-}\
-catch( ... )\
-{\
-    std::cout << "Unspecified Sqrat exception" << std::endl << std::flush;\
-    return;\
-}\
-\
-\
-Sqrat::Function* sqFunc = new Sqrat::Function( Sqrat::RootTable().\
-                         GetFunction( scriptFunctionName.c_str() ) )
-
 ////////////////////////////////////////////////////////////////////////////////
+#define SW_REPETETIVE_SCRIPT_CONNECTIONCODE Sqrat::DefaultVM::Set( vm.getVM() );\
+    \
+    try\
+    {\
+        Sqrat::Script script;\
+        script.CompileFile( scriptPath );\
+        if( runScript )\
+        {\
+            script.Run();\
+        }\
+    }\
+    catch( Sqrat::Exception& e )\
+    {\
+        std::cout << "Sqrat exception: " << e.Message() << std::endl << std::flush;\
+        return;\
+    }\
+    catch( ... )\
+    {\
+        std::cout << "Unspecified Sqrat exception" << std::endl << std::flush;\
+        return;\
+    }\
+    \
+    \
+    Sqrat::Function* sqFunc = new Sqrat::Function( Sqrat::RootTable().\
+                             GetFunction( scriptFunctionName.c_str() ) )
+////////////////////////////////////////////////////////////////////////////////
+#define SW_CONNECTSCRIPT_ARG_LIST bool runScript,\
+    const std::string& signalPattern,\
+    const std::string& scriptFunctionName,\
+    const std::string& scriptPath,\
+    Sqrat::SqratVM& vm,\
+    switchwire::ScopedConnectionList& connections,\
+    switchwire::EventManager::SignalType signalType = switchwire::EventManager::any_SignalType,\
+    switchwire::EventManager::Priority priority = switchwire::EventManager::normal_Priority )
+
+#define SW_CONNECTSIG_ARG_LIST signalPattern,\
+    &Sqrat::Function::Execute,\
+    sqFunc,\
+    connections,\
+    signalType,\
+    priority
+////////////////////////////////////////////////////////////////////////////////
+
+
 // This needs a ScopedScriptsList that stores a smart ptr to the Sqrat::Function
 // so we don't leak that memory
 template< typename Signature >
-void ConnectScript_0( const std::string& signalPattern,
-                      const std::string& scriptFunctionName,
-                      const std::string& scriptPath,
-                      Sqrat::SqratVM& vm,
-                      switchwire::ScopedConnectionList& connections,
-                      switchwire::EventManager::SignalType signalType = switchwire::EventManager::any_SignalType,
-                      switchwire::EventManager::Priority priority = switchwire::EventManager::normal_Priority )
+void ConnectScript_0( SW_CONNECTSCRIPT_ARG_LIST
 {
     SW_REPETETIVE_SCRIPT_CONNECTIONCODE;
 
-    // Connect this "function" as a slot
-    switchwire::ConnectSignals_0< Signature, Sqrat::Function >(
-                    signalPattern,
-                    &Sqrat::Function::Execute,
-                    sqFunc,
-                    connections,
-                    signalType,
-                    priority );
+    switchwire::ConnectSignals_0< Signature, Sqrat::Function >( SW_CONNECTSIG_ARG_LIST );
 }
 ////////////////////////////////////////////////////////////////////////////////
 template< typename Signature >
-void ConnectScript_1( bool runScript,
-                      const std::string& signalPattern,
-                      const std::string& scriptFunctionName,
-                      const std::string& scriptPath,
-                      Sqrat::SqratVM& vm,
-                      switchwire::ScopedConnectionList& connections,
-                      switchwire::EventManager::SignalType signalType = switchwire::EventManager::any_SignalType,
-                      switchwire::EventManager::Priority priority = switchwire::EventManager::normal_Priority )
+void ConnectScript_1( SW_CONNECTSCRIPT_ARG_LIST
 {
     SW_REPETETIVE_SCRIPT_CONNECTIONCODE;
 
-    // Connect this "function" as a slot
-    switchwire::ConnectSignals_1< Signature, Sqrat::Function >(
-                    signalPattern,
-                    &Sqrat::Function::Execute,
-                    sqFunc,
-                    connections,
-                    signalType,
-                    priority );
+    switchwire::ConnectSignals_1< Signature, Sqrat::Function >( SW_CONNECTSIG_ARG_LIST );
+}
+////////////////////////////////////////////////////////////////////////////////
+template< typename Signature >
+void ConnectScript_2( SW_CONNECTSCRIPT_ARG_LIST
+{
+  SW_REPETETIVE_SCRIPT_CONNECTIONCODE;
+
+  switchwire::ConnectSignals_2< Signature, Sqrat::Function >( SW_CONNECTSIG_ARG_LIST );
+}
+////////////////////////////////////////////////////////////////////////////////
+template< typename Signature >
+void ConnectScript_3( SW_CONNECTSCRIPT_ARG_LIST
+{
+    SW_REPETETIVE_SCRIPT_CONNECTIONCODE;
+
+    switchwire::ConnectSignals_3< Signature, Sqrat::Function >( SW_CONNECTSIG_ARG_LIST );
+}
+////////////////////////////////////////////////////////////////////////////////
+template< typename Signature >
+void ConnectScript_4( SW_CONNECTSCRIPT_ARG_LIST
+{
+  SW_REPETETIVE_SCRIPT_CONNECTIONCODE;
+
+  switchwire::ConnectSignals_4< Signature, Sqrat::Function >( SW_CONNECTSIG_ARG_LIST );
+}
+////////////////////////////////////////////////////////////////////////////////
+template< typename Signature >
+void ConnectScript_5( SW_CONNECTSCRIPT_ARG_LIST
+{
+    SW_REPETETIVE_SCRIPT_CONNECTIONCODE;
+
+    switchwire::ConnectSignals_5< Signature, Sqrat::Function >( SW_CONNECTSIG_ARG_LIST );
 }
 ////////////////////////////////////////////////////////////////////////////////
 //template< class Signature >
