7fefbbf8c74b23b50f8bc8daf82b28d07b3d61be
doug.mccorkle@gmail.com <doug.mccorkle@gmail.com@00c3c29d-d336-5f76-5ff7-ebc043f3ed38>
2014-01-19 01:33:05 +0000

    update the build files to automatically apply a d for debug builds.

--- CMakeModules/VESSetupCompiler.cmake
+++ CMakeModules/VESSetupCompiler.cmake
@@ -1,20 +1,21 @@
 ################################################################################
 # Platform specific definitions
-IF(WIN32)
-    IF(MSVC)
+if(WIN32)
+    if(MSVC)
         # To enable /MP, parralel build on MSVC
         OPTION(WIN32_USE_MP "Set to OFF to diable /MP." ON)
         MARK_AS_ADVANCED(WIN32_USE_MP)
-        IF(WIN32_USE_MP)
-            SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /MP")
-        ENDIF(WIN32_USE_MP)
+        if(WIN32_USE_MP)
+            set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /MP")
+        endif(WIN32_USE_MP)
 
         # Other MSVC compilation flags
         #ADD_DEFINITIONS(-D_SCL_SECURE_NO_WARNINGS)
         #ADD_DEFINITIONS(-D_CRT_SECURE_NO_DEPRECATE)
 
-    ENDIF(MSVC)
-ENDIF (WIN32)
+        set( CMAKE_DEBUG_POSTFIX d )
+    endif(MSVC)
+endif(WIN32)
 
 if(CMAKE_BUILD_TOOL MATCHES "(msdev|devenv|nmake)")
     add_definitions(/W2)
@@ -31,4 +32,3 @@ elseif(CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX)
   # Update if necessary
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra")# -Wpedantic")
 endif()
-
